963294a42a8f934163ee5fdd874a7609752a56b1
jserweta <44199012+jserweta@users.noreply.github.com>
2018-11-06 06:02:19 +0100

    Create Triangle.java

---
+++ Lab2/Shapes/Triangle.java
@@ -0,0 +1,13 @@
+public class Triangle extends Shape {
+    private int base;
+
+    public Triangle(int base, String name){
+        this.base=base;
+        this.name=name;
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
