3224d0a1f3b3248cf61f71b269115908c8d658d0
jserweta <44199012+jserweta@users.noreply.github.com>
2018-11-06 06:02:38 +0100

    Create Circle.java

---
+++ Lab2/Shapes/Circle.java
@@ -0,0 +1,14 @@
+public class Circle extends Shape {
+    private int radius;
+
+    public Circle(int radius, String name){
+        this.radius=radius;
+        this.name=name;
+    }
+
+
+    @Override
+    public void draw() {
+
+    }
+}
