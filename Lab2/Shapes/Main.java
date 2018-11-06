a8334bad3c1c1eedea41d8754af56a6ccef44951
jserweta <44199012+jserweta@users.noreply.github.com>
2018-11-06 06:36:18 +0100

    Create Main.java

---
+++ Lab2/Shapes/Main.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 
+        LinkedList<Shape> ShapeList = new LinkedList<Shape>();
+        ShapeList.add(new Rectangle());
+        ShapeList.add(new Circle());
+        ShapeList.add(new Triangle());
+
+        for(Shape i: ShapeList){
+            i.draw();
+        }
+    }
+}
