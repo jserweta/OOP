7afe004f8389bd9c73e682000135530c107060af
jserweta <44199012+jserweta@users.noreply.github.com>
2018-11-06 06:22:11 +0100

    Update Circle.java

--- Lab2/Shapes/Circle.java
+++ Lab2/Shapes/Circle.java
@@ -1,14 +1,6 @@
 public class Circle extends Shape {
-    private int radius;
-
-    public Circle(int radius, String name){
-        this.radius=radius;
-        this.name=name;
-    }
-
-
     @Override
     public void draw() {
-
+        System.out.print("Koło\n");
     }
 }
