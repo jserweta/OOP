4c5208784635a428bbb4acb342e8c3990968bcb0
jserweta <44199012+jserweta@users.noreply.github.com>
2018-11-06 15:07:55 +0100

    Create Calculator.java

---
+++ Kolokwium1/Calculator.java
@@ -0,0 +1,20 @@
+package appl;
+import java.io.File;
+
+public abstract class Calculator {
+    public String result = "jeden";
+    public String x="dwa";
+     public void SaveToFile(String x, String filename){
+         File plik=new File("filename.txt");
+    }
+
+    public void Add(String x){
+
+    }
+    public void Subtract(String x){
+
+    }
+    public void Multipy(int x){
+
+    }
+}
