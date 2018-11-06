f065fad18159d61544a3b2f8afa9ce1b5d9e2e37
jserweta <44199012+jserweta@users.noreply.github.com>
2018-11-06 06:22:31 +0100

    Update Rectangle.java

--- Lab2/Shapes/Rectangle.java
+++ Lab2/Shapes/Rectangle.java
@@ -1,22 +1,6 @@
 public class Rectangle extends Shape {
-    protected int aLength;
-    protected int bLength;
-
-
-    public Rectangle(int aLength, int bLength, String name){
-        this.aLength=aLength;
-        this.bLength=bLength;
-        this.name=name;
-    }
-
-    public Rectangle() {
-        aLength=1;
-        bLength=1;
-        name="";
-    }
-
     @Override
     public void draw() {
-
+        System.out.print("Prostokąt\n");
     }
 }
