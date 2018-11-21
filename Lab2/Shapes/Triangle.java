a4552e019e98ad6b571cfe2d03439d0b49409093
Jakub Serweta <jakub.serweta@gmail.com>
2018-11-21 01:02:20 +0100

    Extended Shapes

--- Lab2/Shapes/Triangle.java
+++ Lab2/Shapes/Triangle.java
@@ -1,4 +1,5 @@
 public class Triangle extends Shape {
+
     private int base;
 
     public Triangle(int base, String name){
@@ -8,6 +9,14 @@ public class Triangle extends Shape {
 
     @Override
     public void draw() {
-
+        for(int i = 0; i < base + 1; i++){
+            for(int j = base; j > i; j--){
+            System.out.print(" ");
+            }
+            for(int k = 0; k < (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
