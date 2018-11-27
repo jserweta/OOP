5ac4ec3b657293574d719077ba971921fe9da7b3
jserweta <jakub.serweta@gmail.com>
2018-11-27 02:10:17 +0100

    Crypto update

--- Lab4/Cryptography/ROT11.java
+++ Lab4/Cryptography/ROT11.java
@@ -1,11 +1,68 @@
 public class ROT11 implements Algorithm{
+
+    private final int move = 11;
+    private static final char[] alphabet= { 'a', 'b', 'c', 'd', 'e',
+            'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E',
+            'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
     @Override
     public String crypt(String word) {
-        return null;
+
+        char[] values = word.toCharArray();
+        for (int i = 0; i < values.length; i++) {
+            char letter = values[i];
+
+            if (letter >= alphabet[0] && letter<= alphabet[25]) {
+
+                if (letter > alphabet[10]) {
+                    letter -= move;
+                } else {
+                    letter += move;
+                }
+            } else if (letter >= alphabet[26] && letter <= alphabet[51]) {
+
+                if (letter > alphabet[36]) {
+                    letter -= move;
+                } else {
+                    letter += move;
+                }
+            }
+            values[i] = letter;
+        }
+        String encryptedWord = new String(values);
+
+        return encryptedWord;
+
     }
 
     @Override
     public String decrypt(String word) {
-        return null;
+
+        char[] values = word.toCharArray();
+        for (int i = 0; i < values.length; i++) {
+            char letter = values[i];
+
+            if (letter >= alphabet[0] && letter <= alphabet[25]) {
+
+                if (letter > alphabet[12]) {
+                    letter -= move;
+                } else {
+                    letter += move;
+                }
+            } else if (letter >= alphabet[26] && letter <= alphabet[51]) {
+
+                if (letter > alphabet[38]) {
+                    letter -= move;
+                } else {
+                    letter += move;
+                }
+            }
+            values[i] = letter;
+        }
+        String decryptedWord = new String(values);
+        
+        return decryptedWord;
     }
 }
